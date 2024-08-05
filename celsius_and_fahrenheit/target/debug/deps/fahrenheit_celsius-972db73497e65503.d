b8171a493a8934c1d1443db32e01905b109c8fba
dot-richard <dot-richard@users.noreply.github.com>
2024-08-05 13:15:38 +0200

    rm .gitignore

---
+++ celsius_and_fahrenheit/target/debug/deps/fahrenheit_celsius-972db73497e65503.d
@@ -0,0 +1,5 @@
+/home/richard/dev/rust/exercices/fahrenheit_celsius/target/debug/deps/fahrenheit_celsius-972db73497e65503: src/main.rs
+
+/home/richard/dev/rust/exercices/fahrenheit_celsius/target/debug/deps/fahrenheit_celsius-972db73497e65503.d: src/main.rs
+
+src/main.rs:
