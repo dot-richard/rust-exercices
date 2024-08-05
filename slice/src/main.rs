c3c52eff33076560af7da5dbaca00a9e22f73309
dot-richard <dot-richard@users.noreply.github.com>
2024-08-05 14:18:51 +0200

    slice first word

--- slice/src/main.rs
+++ slice/src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+// ne fonctionne qu'avec l'ascii
+fn first_word(s: &String) -> &str {
+    let octets = s.as_bytes();
+
+    for (i, &element) in octets.iter().enumerate() {
+        if element == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
 }
+
+fn main() {
+    let mut s = String::from("Hello world !");
+    let first_word = first_word(&s);
+
+    // s.clear(); // error !
+
+    println!("string: '{}'", s);
+    println!("first_word: '{}'", first_word);
+
+    s.clear();
+}
+
+
