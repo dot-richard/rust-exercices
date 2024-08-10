77508c00b785ff5a08e874c50778fc15114bcfe4
dot-richard <dot-richard@users.noreply.github.com>
2024-08-10 10:35:34 +0200

    fin de l'exercice

--- entreprise/src/main.rs
+++ entreprise/src/main.rs
@@ -11,37 +11,7 @@
 use std::io;
 use std::collections::HashMap;
 
-#[derive(Debug)]
-struct Company {
-    departments: HashMap<String, Vec<String>>,
-    employees: Vec<String>,
-}
-
-impl Company {
-    fn new() -> Self {
-        Company {
-            departments: HashMap::new(),
-            employees: Vec::new()
-        }
-    }
-
-    fn add_department(&mut self, department: String) {
-        self.departments.entry(department).or_insert(Vec::new());
-    }
-
-    fn add_employee(&mut self, employee: String) {
-        self.employees.push(employee);
-    }
-
-    fn show(&self, pretty: Option<bool>) {
-        if pretty != None {
-            println!("{:#?}", self);
-        } else {
-            println!("{:?}", self);
-        }
-    }
-}
-
+// retourner une entrée utilisateur
 fn cli_read_line() -> String {
     let mut input = String::new();
 
@@ -52,25 +22,91 @@ fn cli_read_line() -> String {
     input.trim().to_string()
 }
 
-fn cli_add_employee(company: &mut Company) {
+fn cli_add_employee_to_department(
+    departments: &mut HashMap<String, Vec<String>>,
+    employees: &Vec<String>) {
+
+    println!("------------------");
     println!("Ajouter un employé");
+
+    // employés
+
+    println!();
+    println!("Saisissez un employé :");
+    println!("ENTER pour arrêter.");
+    for employee in employees {
+        println!("{}", employee);
+    }
 
     let employee = cli_read_line();
 
-    if !employee.is_empty() {
-        company.add_employee(employee.to_string());
-        println!("Le nouvel employé {} a bien été ajouté.", employee);
+    if employee.is_empty() {
+        println!("Prénom de l'employé vide.");
+        return;
     }
 
+    // departements
+
+    println!();
+    println!("Saisissez un departement :");
+    println!("ENTER pour arrêter.");
+
+    for key in departments.keys() {
+        println!("{}", key);
+    }
+
+    let department = cli_read_line();
+
+    if department.is_empty() {
+        println!("Nom du département vide.");
+        return;
+    }
+
+    // ajout
+
+    if let Some(employees) = departments.get_mut(&department) {
+        employees.push(employee);
+        if let Some(employee) = employees.last() {
+            println!(
+                "Le nouvel employé '{}' a été ajouté au département '{}' avec succès.",
+                employee, department
+            );
+        }
+    } else {
+        println!("Département '{}' inexistant.", department);
+    }
 }
 
-fn cli_show_employees(company: &Company) {
-    println!("Voir les employés");
-    println!("{:?}", company.employees);
+fn cli_show_employees_for_department(departments: &HashMap<String, Vec<String>>) {
+    println!("Saisissez un departement :");
+
+    for dep in departments.keys() {
+        println!("{}", dep);
+    }
+
+    let dep = cli_read_line();
+
+    if let Some(employees) = departments.get(&dep) {
+        cli_show_employees(&employees);
+    } else {
+        println!("Le departement '{}' est inexistant", dep);
+    }
 }
 
-fn cli_show_department_employees(company: &Company) {
-    println!("Voir les employés d'un departement");
+fn cli_show_all(departments: &HashMap<String, Vec<String>>) {
+    for dep in departments.keys() {
+        println!("{}", dep);
+        cli_show_employees(&departments[dep])
+    }
+}
+
+fn cli_show_employees(employees: &Vec<String>) {
+    let mut employees = employees.clone();
+    employees.sort();
+
+    for emp in employees {
+        println!("- {}", emp);
+    }
 }
 
 fn main() {
@@ -80,48 +116,45 @@ fn main() {
 
     // initialisation de données par defaut
 
-    let employees = [
+    let data_departments: Vec<String> = vec![
+        "service technique",
+        "service commercial",
+        "ressource humaine",
+        "bureau d'étude"
+    ].iter().map(|&s| s.to_string()).collect();
+
+    let data_employees: Vec<String> = vec![
         "Richard",
         "Léa",
         "Mathieu",
         "Anne",
         "Jean-Christophe",
         "Julie",
-    ];
+    ].iter().map(|&s| s.to_string()).collect();
 
-    let departments = [
-        "service technique",
-        "service commercial",
-        "ressource humaine",
-        "bureau d'étude"
-    ];
+    let mut departments: HashMap<String, Vec<String>> = HashMap::new();
 
-    let mut company: Company = Company::new();
-
-    for department in departments {
-        company.add_department(department.to_string());
-    }
-
-    for employee in employees {
-        company.add_employee(employee.to_string());
+    for department in data_departments {
+        departments.entry(department.to_string()).or_insert(Vec::new());
     }
 
     // menu
 
     loop {
         println!();
-        println!("Choisissez une option:");
+        println!("Choisissez une option :");
         println!("1. Ajouter un employé");
-        println!("2. Afficher les employés d'un departement");
-        println!("3. Afficher tout les employés.");
+        println!("2. Afficher les employés d'un département");
+        println!("3. Afficher tout les employés de tout les département");
         println!("ENTER pour Quitter");
+        println!();
 
         let input = cli_read_line();
 
         match input.as_str() {
-            "1" => cli_add_employee(&mut company),
-            "2" => cli_show_department_employees(&company),
-            "3" => cli_show_employees(&company),
+            "1" => cli_add_employee_to_department(&mut departments, &data_employees),
+            "2" => cli_show_employees_for_department(&departments),
+            "3" => cli_show_all(&departments),
             _   => break
         };
     }
